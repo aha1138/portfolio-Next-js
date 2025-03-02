ed21404ad06d014f65537553a011108e09345b99
aha1138 <ahguj@ulaval.ca>
2025-03-02 17:01:58 -0500

    feat add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
 import FloatingNavBar from '../components/FloatingNavBar';
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({subsets: ["latin"]});
 
@@ -28,6 +29,7 @@ export default function RootLayout({
           >
           {children}
           <FloatingNavBar />
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
